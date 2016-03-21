3f65d15107999fd98ef33389f2a3db4c58bbfb82
bxagha <bxagha@gmail.com>
2016-03-21 23:49:29 +0300

    Добавил конструктор(формирует имя = тип класса, возраст случайный от 0 до 9) и методыполучения имени и возраста. Убрал переменную типа класса за ненадобностью.

--- src/Animals.java
+++ src/Animals.java
@@ -1,10 +1,27 @@
+import java.util.Random;
+
 /**
  * Created by Сергей on 20.03.2016.
  */
 public class Animals {
     protected String name;
     protected int age;
-    protected String type;
+//    private static int count=0;
+    public static Random rand = new Random();
+
+    public Animals() {
+//        count++;
+        name = this.getClass().getName();
+        age = rand.nextInt(10);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
 
     public void feed(){
         
