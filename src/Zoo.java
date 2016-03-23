f1aaac1f5bad656760fee0a198566ea64e8ca57f
bxagha <bxagha@gmail.com>
2016-03-23 11:09:40 +0300

    Добавил цикл кормления случайного животного случайной едой

--- src/Zoo.java
+++ src/Zoo.java
@@ -6,13 +6,35 @@ import java.util.ArrayList;
 public class Zoo {
     public static final int COUNT_ANIMALS = 50;
     public static Pet[] pets = new Pet[COUNT_ANIMALS];
-    public enum Food{grass, water, mouse, bone, foliage}
+
+    public enum Food {grass, water, mouse, bone, foliage}
 
     public static void main(String[] args) {
 
         ArrayList<Cell> cells = new ArrayList<Cell>(); //строим зоопарк из безразмерных клеток
         petIntoZoo(cells);//заселяем зоопарк животными
         cells.forEach(Cell::showCell);//выводим в консоль все клетки с животными
+        for (int i = 0; i < 50; i++) {//кормим случайное животное случайной едой
+            randomPet().getPet().feed(randomFood());
+        }
+    }
+
+    private static Food randomFood() {//выбираем случайную еду
+        switch (Animals.rand.nextInt(5)) {
+            case 1:
+                return Food.water;
+            case 2:
+                return Food.mouse;
+            case 3:
+                return Food.bone;
+            case 4:
+                return Food.foliage;
+        }
+        return Food.grass;
+    }
+
+    private static Pet<?> randomPet() { //выбираем случайное животное
+        return pets[Animals.rand.nextInt(pets.length)];
     }
 
     private static void petIntoZoo(ArrayList<Cell> cells) {
@@ -33,8 +55,6 @@ public class Zoo {
 
     public static Pet<?> createPet() {//создаем случайное жЫвотное
         switch (Animals.rand.nextInt(5)) {
-            case 0:
-                return new Pet<>(new Giraffe());
             case 1:
                 return new Pet<>(new Cat());
             case 2:
@@ -44,6 +64,6 @@ public class Zoo {
             case 4:
                 return new Pet<>(new Elephant());
         }
-        return new Pet<>(new Cat());
+        return new Pet<>(new Giraffe());
     }
 }
